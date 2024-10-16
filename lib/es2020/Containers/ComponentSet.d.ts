44095b254561d10a7b6e7cb9d416989fc9e94ba9
benten <ben@benten.ca>
2024-10-16 16:11:46 -0700

    fix: SparseSet -> ComponentSet, fix init issue

--- lib/es2020/Containers/ComponentSet.d.ts
+++ lib/es2020/Containers/ComponentSet.d.ts
@@ -6,61 +6,18 @@
  * This means it is possible to look up the index of a value in constant time.
  * However, the set does not guarantee the order of the values.
  */
-export declare class ComponentSet<T> {
+export declare class ComponentSet<T> extends Set<T> {
+    #private;
     /**
      * Returns the first element in the set or undefined if the set is empty.
+     * `O(1)` complexity.
      */
     get first(): T | undefined;
     /**
      * Returns the last element in the set or undefined if the set is empty.
+     * Potentially `O(n)` complexity.
      */
     get last(): T | undefined;
-    /**
-     * Adds a value to the set.
-     * @param value The value to add.
-     */
-    add(value: T): void;
-    /**
-     * Removes a value from the set.
-     * @param value The value to remove.
-     */
-    delete(value: T): void;
-    /**
-     * Checks if a value is in the set.
-     * @param value The value to check.
-     * @returns True if the value is in the set, false otherwise.
-     */
-    has(value: T): boolean;
-    /**
-     * Returns an iterator over the values in the set.
-     * @returns An iterator over the values in the set.
-     */
-    values(): IterableIterator<T>;
-    /**
-     * Map the values of the set to a new array.
-     * @param callback
-     */
-    map<U>(callback: (value: T) => U): U[];
-    /**
-     * Iterate over the values of the set.
-     * @param callback
-     */
-    forEach(callback: (value: T) => void): void;
-    /**
-     * Filter the values of the set.
-     * @param callback
-     */
-    filter(callback: (value: T) => boolean): T[];
-    /**
-     * Returns the value at the given index in the set.
-     * @param index The index of the value to return.
-     * @returns The value at the given index, or undefined if the index is out of bounds.
-     */
-    at(index: number): T | undefined;
-    [Symbol.iterator](): Generator<T, void, unknown>;
-    /** * Clears the set */
-    clear(): void;
-    get length(): number;
-    private dense;
-    private sparse;
+    add(value: T): this;
+    delete(value: T): boolean;
 }
